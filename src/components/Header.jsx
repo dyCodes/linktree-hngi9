082f347001feb01f00b899ed7162722436c8a371
dyCodes <yusufdaudu51@gmail.com>
2022-10-30 01:39:13 +0100

    Add share button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import profile from "../assets/profile.png";
 import shareIcon from "../assets/share-button.png";
+import dotsIcon from "../assets/dot.png";
 
 const Header = () => {
   return (
@@ -7,6 +8,11 @@ const Header = () => {
       <img id="profile_img" src={profile} alt="Yusuf Daudu" />
       <h1 id="twitter" className="username">dyCodes</h1>
       <p id="slack" hidden>dyCodes</p>
+
+      <div className="share-button">
+        <img src={shareIcon} className="pc" alt="share" />
+        <img src={dotsIcon} className="mobile" alt="share" />
+      </div>
     </header>
   )
 }
--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -3,16 +3,11 @@ const Links = () => {
   return (
     <div className="links">
       <ButtonLink id="twitter" url="https://twitter.com/dycodes" text="Twitter Link" />
-
       <ButtonLink id="btn__zuri" url="https://training.zuri.team/" text="Zuri Team" />
-
       <ButtonLink id="books" url="https://books.zuri.team/" text="Zuri Books" />
-
-      <ButtonLink id="book__python" url="https://books.zuri.team/python-for-beginners?ref_id=dyCodes"
-        text="Python Books" />
-
+      <ButtonLink id="book__python"
+        url="https://books.zuri.team/python-for-beginners?ref_id=dyCodes" text="Python Books" />
       <ButtonLink id="pitch" url="https://background.zuri.team/" text="Background Check for Coders" />
-
       <ButtonLink id="book__design" url="https://books.zuri.team/design-rules" text="Design Books" />
     </div >
   )
@@ -20,7 +15,7 @@ const Links = () => {
 
 const ButtonLink = ({ id, url, text }) => {
   return (
-    <a href={url} id={id} className="btn link">{text}</a>
+    <a href={url} id={id} className="btn link" target={"_blank"} rel="noreferrer">{text}</a>
   );
 
 }
